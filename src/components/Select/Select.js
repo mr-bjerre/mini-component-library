99d3a02133bae6d3963f385a69896a89deb93fd3
Nicolai Bjerre Pedersen <mr.bjerre@gmail.com>
2021-10-02 21:59:48 +0200

    Solve select except dynamic width

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <ChevronWrapper>
+        <Icon id="chevron-down" strokeWidth="2" />
+      </ChevronWrapper>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+    </Wrapper>
   );
 };
+
+const StyledSelect = styled.select`
+  appearance: none;
+  border: 0;
+  padding: 12px 16px;
+  padding-right: 52px;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: currentColor;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: inline;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: black;
+  }
+`;
+
+const ChevronWrapper = styled.div`
+  position: absolute;
+  right: 16px;
+  top: -1px;
+`;
 
 export default Select;
