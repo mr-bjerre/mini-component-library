bf0778bd739c5529204854f910ddacc1f97c04f1
Nicolai Bjerre Pedersen <mr.bjerre@gmail.com>
2021-10-02 21:17:40 +0200

    Solve ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,58 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--border-radius": "4px",
+  },
+  medium: {
+    "--height": "16px",
+    "--border-radius": "4px",
+  },
+  large: {
+    "--height": "24px",
+    "--padding": "4px",
+    "--border-radius": "8px",
+  },
+};
+
+const Wrapper = styled.div`
+  height: var(--height);
+  padding: var(--padding);
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--border-radius);
+  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+`;
+
+const StyledProgressBar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${(p) => `${p.value}%`};
+  height: 100%;
+  border-radius: var(--border-radius)
+    ${(p) =>
+      p.value < 100 ? "0 0" : "var(--border-radius) var(--border-radius)"}
+    var(--border-radius);
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper style={SIZES[size]}>
+      <StyledProgressBar
+        role="progressbar"
+        value={value}
+        style={SIZES[size]}
+        aria-valuenow={value}
+        aria-valuemin="0"
+        aria-valuemax="100"
+      />
+      <VisuallyHidden>Progress bar</VisuallyHidden>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
