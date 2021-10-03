4551106024919a50cdeb4f203a7f54ba10867c35
Nicolai Bjerre Pedersen <mr.bjerre@gmail.com>
2021-10-03 15:51:50 +0200

    Fix select after watching video

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -10,40 +10,56 @@ const Select = ({ label, value, onChange, children }) => {
 
   return (
     <Wrapper>
-      <ChevronWrapper>
-        <Icon id="chevron-down" strokeWidth="2" />
-      </ChevronWrapper>
-      <StyledSelect value={value} onChange={onChange}>
+      <NativeSelect value={value} onChange={onChange}>
         {children}
+      </NativeSelect>
+      <StyledSelect>
+        {displayedValue}
+        <ChevronWrapper>
+          <Icon id="chevron-down" strokeWidth="2" size={24} />
+        </ChevronWrapper>
       </StyledSelect>
     </Wrapper>
   );
 };
 
-const StyledSelect = styled.select`
-  appearance: none;
-  border: 0;
+const NativeSelect = styled.select`
+  position: absolute;
+  opacity: 0;
+  height: 100%;
+  width: 100%;
+`;
+
+const StyledSelect = styled.div`
   padding: 12px 16px;
   padding-right: 52px;
   background-color: ${COLORS.transparentGray15};
   border-radius: 8px;
-  color: currentColor;
-`;
 
-const Wrapper = styled.div`
-  position: relative;
-  display: inline;
-  color: ${COLORS.gray700};
+  ${NativeSelect}:focus + & {
+    outline: 5px auto -webkit-focus-ring-color;
+  }
 
-  &:hover {
+  ${NativeSelect}:hover + & {
     color: black;
   }
 `;
 
+const Wrapper = styled.div`
+  position: relative;
+  color: ${COLORS.gray700};
+  width: fit-content;
+`;
+
 const ChevronWrapper = styled.div`
   position: absolute;
-  right: 16px;
-  top: -1px;
+  right: 12px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  height: 24px;
+  width: 24px;
+  pointer-events: none;
 `;
 
 export default Select;
